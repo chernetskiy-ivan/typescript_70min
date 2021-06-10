bff5dd8e73f3ead5099a835ff3fc9eabff71b33c
Ivan <ivanchernetskey@mail.ru>
2021-06-10 14:17:37 +0300

    interface and implements

--- 2_interface.ts
+++ 2_interface.ts
@@ -37,3 +37,18 @@ const rect5: RectWithArea = {
         return this.size.width * this.size.height
     }
 }
+
+// =============
+
+interface IClock {
+    time: Date
+    setTime(date: Date): void
+}
+
+class Clock implements IClock {
+    time: Date = new Date()
+
+    setTime(date: Date): void {
+        this.time = date
+    }
+}
