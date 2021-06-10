0b62359feea6e4c6007c6a0217fe0e22bc31c6ce
Ivan <ivanchernetskey@mail.ru>
2021-06-10 12:16:40 +0300

    basic types

--- 1 types.ts
+++ 1 types.ts
@@ -1,3 +1,13 @@
-const str: string = 'Hello world'
+const isFetching: boolean = true
+const isLoading: boolean = false
 
-console.log(str)
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'hello TypeScript'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['hello', 'typescript']
